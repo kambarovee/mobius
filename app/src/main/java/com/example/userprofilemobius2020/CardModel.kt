183dea2dc55067f37ee973848739ae2042474d7d
Yerkebulan Kambarov <44515882+kambarovee@users.noreply.github.com>
2021-03-21 04:05:05 +0600

    Added viewpager

---
+++ app/src/main/java/com/example/userprofilemobius2020/CardModel.kt
@@ -0,0 +1,9 @@
+package com.example.userprofilemobius2020
+
+class CardModel(
+    var iconImage: Int,
+    var title: String,
+    var secondaryTitle: String,
+    var secondarySubTitle: String
+) {
+}
--- app/src/main/java/com/example/userprofilemobius2020/MainActivity.kt
+++ app/src/main/java/com/example/userprofilemobius2020/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.userprofilemobius2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vpAdapter: ViewPagerAdapter
+    private lateinit var modelList: ArrayList<CardModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadCards()
+    }
+
+    private fun loadCards() {
+        modelList = ArrayList()
+
+        modelList.add(CardModel(R.drawable.icon_sberprime, "SberPrime", "asDQWedsa", "sadASd"))
+        modelList.add(CardModel(R.drawable.icon_sberprime, "Perevodi", "ASDqwe", "QAswqVadsq"))
+        modelList.add(CardModel(R.drawable.icon_sberprime, "SberPrime", "asDQWedsa", "sadASd"))
+
+        vpAdapter = ViewPagerAdapter(this, modelList)
+
+        vp_cardview.adapter = vpAdapter
+
+//        vp_cardview.setPadding(0,0,100,0)
+
     }
 }
